63c1a06367075f76b074b80cd9ceb05cb1e348af
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-12 11:24:04 +0500

    Added as a Parent Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/appstyle.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/home";
+import About from "./Components/About";
+import Error from "./Components/Error";
+import Navbar from "./Components/navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
