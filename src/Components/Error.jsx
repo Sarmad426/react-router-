583dec515274a27c8d20f285cdb15b0c3df7ac40
Sarmad Rafique <sarmadrafique040@gmail.com>
2022-10-12 11:16:46 +0500

    Added an Error Page for non existing pages

---
+++ src/Components/Error.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+class Error extends React.Component {
+  render() {
+    return (
+      <div className="about">
+        <h3>Page not Found for This Url:</h3>
+        <Link to="/" className="btn">
+          Back Home
+        </Link>
+      </div>
+    );
+  }
+}
+export default Error;
